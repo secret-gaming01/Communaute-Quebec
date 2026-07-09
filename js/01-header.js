e7f8f5943cc28258f59dd62c81cf49ed752b69f4
theorete12-web <theorete12-web@users.noreply.github.com>
2026-07-08 22:29:34 -0400

    Mise à jour du site

--- js/01-header.js
+++ js/01-header.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const header = `
-        <header>
-            <nav>
-                <a href="../Pages/01-accueil.html">Accueil</a>
-                <a href="../Pages/02-staff.html">Staff</a>
-                <a href="../Pages/03-activites.html">Activités</a>
-                <a href="../Pages/04-reseaux.html">Réseaux</a>
-                <a href="../Pages/05-reglement.html">Règlement</a>
-                <a href="../Pages/06-support.html">Support</a>
-                <a href="../Pages/07-securite.html">Sécurité</a>
-                <a href="../Pages/08-faq.html">FAQ</a>
-                <a href="../Pages/09-actualites.html">Actualités</a>
-                <a href="../Pages/10-avis.html">Avis</a>
+        <header id="header">
+            <nav class="nav">
+                <ul class="nav-links">
+                    <li><a href="../Pages/01-accueil.html">Accueil</a></li>
+                    <li><a href="../Pages/02-staff.html">Staff</a></li>
+                    <li><a href="../Pages/03-activites.html">Activités</a></li>
+                    <li><a href="../Pages/04-reseaux.html">Réseaux</a></li>
+                    <li><a href="../Pages/05-reglement.html">Règlement</a></li>
+                    <li><a href="../Pages/06-support.html">Support</a></li>
+                    <li><a href="../Pages/07-securite.html">Sécurité</a></li>
+                    <li><a href="../Pages/08-faq.html">FAQ</a></li>
+                    <li><a href="../Pages/09-actualites.html">Actualités</a></li>
+                    <li><a href="../Pages/10-avis.html">Avis</a></li>
+                </ul>
             </nav>
         </header>
     `;
